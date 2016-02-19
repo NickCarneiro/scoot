4aed8da4c539c74870374bc9f4e4e8c97095bad2
Nick Carneiro <nickc@trillworks.com>
2016-02-19 12:26:38 -0800

    put all the scoots on a map

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, send_from_directory
+import json
+from flask import Flask, render_template, send_from_directory, jsonify
 from flask_bootstrap import Bootstrap
 from ScootLocations import ScootLocations
 
@@ -7,14 +8,23 @@ app.debug = True
 Bootstrap(app)
 
 @app.route("/")
-def hello():
+def index():
+    return render_template('index.html')
+
+@app.route('/api/scooters')
+def scooters():
     scoot_locations = ScootLocations('downloads/2016-02-19_01:40:07.json')
-    scooters = scoot_locations.get_scooters()
-    return render_template('index.html', scooters=scooters)
+    scooter_list = scoot_locations.get_scooters()
+    scooter_json = json.dumps(scooter_list)
+    return scooter_json
 
 @app.route('/static/js/<path:path>')
 def send_js(path):
     return send_from_directory('static/js', path)
+
+@app.route('/static/css/<path:path>')
+def send_css(path):
+    return send_from_directory('static/css', path)
 
 if __name__ == "__main__":
     app.run()
--- static/js/scoot.js
+++ static/js/scoot.js
@@ -1,1 +1,46 @@
-console.log('scoot');
+var mapElem = document.getElementById('map');
+var windowHeight = window.innerHeight - 60;
+mapElem.style.height = windowHeight + 'px';
+var map;
+function initMap() {
+    console.log('initializing map');
+    map = new google.maps.Map(mapElem, {
+        center: {lat: 37.765266, lng: -122.443355},
+        zoom: 14
+    });
+
+    var request = new XMLHttpRequest();
+    request.open('GET', '/api/scooters', true);
+
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            // Success!
+            var resp = request.responseText;
+            var scooters = JSON.parse(resp);
+            scooters.forEach(function(scooter) {
+                var location = {lat: parseFloat(scooter.latitude), lng: parseFloat(scooter.longitude)};
+                new google.maps.Marker({
+                    position: location,
+                    map: map,
+                    title: String(scooter.physical_scoot_id)
+                });
+            });
+        } else {
+            // We reached our target server, but it returned an error
+           console.log('error fetching scooters')
+        }
+    };
+
+    request.onerror = function(e) {
+        // There was a connection error of some sort
+        console.log(e);
+    };
+
+    request.send();
+
+
+}
+
+
+
+
