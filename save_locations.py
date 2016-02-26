8ca0a84c482fd0d34ee50723328c1a2182232a61
Nick Carneiro <nickc@trillworks.com>
2016-02-25 17:28:26 -0800

    fix ssl verification error of scoot cert, fixes #1

--- save_locations.py
+++ save_locations.py
@@ -12,7 +12,7 @@ rollbar.init(os.environ['ROLLBAR_TOKEN'], environment)  # access_token, environm
 scoot_location_url = 'https://app.scootnetworks.com/api/v1/scooters.json?locations_updated_since=null'
 now_unix_timestamp = int(time.time())
 try:
-    r = requests.get(scoot_location_url)
+    r = requests.get(scoot_location_url, verify=False)
     output_file_name = datetime.datetime.fromtimestamp(now_unix_timestamp).strftime('%Y-%m-%d_%H:%M:%S')
     output_file_name += '.json'
     print output_file_name
